e63b2a55597d5c7c7c3ecb0fa11f7a97af9b90b2
mattiavolpe <64392380+mattiavolpe@users.noreply.github.com>
2023-10-16 14:00:02 +0200

    ADD updatePizza api route

--- src/main/java/org/java/app/api/PizzaRestController.java
+++ src/main/java/org/java/app/api/PizzaRestController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,24 +30,24 @@ public class PizzaRestController {
 	private PizzaService pizzaService;
 	
 	@GetMapping
-	public ResponseEntity<List<Pizza>> getAll() {
+	public ResponseEntity<List<Pizza>> getAllPizzas() {
 		List<Pizza> pizzas = pizzaService.findAll();
 		
-		return new ResponseEntity<>(pizzas, HttpStatus.OK);
+		return new ResponseEntity<List<Pizza>>(pizzas, HttpStatus.OK);
 	}
 	
 	@GetMapping("/filter/{filter}")
-	public ResponseEntity<List<Pizza>> getFiltered(@PathVariable String filter) {
+	public ResponseEntity<List<Pizza>> getFilteredPizzas(@PathVariable String filter) {
 		List<Pizza> filteredPizzas = pizzaRepo.findByNameContaining(filter);
 		
-		return new ResponseEntity<>(filteredPizzas, HttpStatus.OK);
+		return new ResponseEntity<List<Pizza>>(filteredPizzas, HttpStatus.OK);
 	}
 	
-	@PostMapping("{id}")
+	@PostMapping("/{id}")
 	public ResponseEntity<Pizza> getPizza(@PathVariable int id) {
 		Pizza pizza = pizzaService.findById(id);
 		
-		return new ResponseEntity<>(pizza, HttpStatus.OK);
+		return new ResponseEntity<Pizza>(pizza, HttpStatus.OK);
 	}
 	
 	@PostMapping
@@ -56,5 +57,15 @@ public class PizzaRestController {
 		pizzaService.savePizza(pizza);
 		
 		return new ResponseEntity<>(pizza.getId(), HttpStatus.OK);
+	}
+	
+	@PutMapping("{id}")
+	public ResponseEntity<Pizza> updatePizza(@PathVariable int id, @RequestBody PizzaDto pizzaDto) {
+		Pizza pizza = new Pizza(pizzaDto);
+		pizza.setId(id);
+		
+		pizzaService.savePizza(pizza);
+		
+		return new ResponseEntity<Pizza>(pizza, HttpStatus.OK);
 	}
 }
